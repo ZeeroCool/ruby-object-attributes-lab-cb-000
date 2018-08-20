096c58e1cf3433a630fc759b23a12e1fe9d77a12
David Z <davidzabran@gmail.com>
2018-08-20 17:34:22 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+
+  initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def name=
+    @name = new_name
+  end
+  
+  def breed
+    
+  end
+
+  def breed=
+    
+  end
+
+
+end 
